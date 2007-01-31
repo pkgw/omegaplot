a6b9d2abc2f1846a6181eed6b720aff7693cd034
Peter Williams <peter@newton.cx>
2007-01-31 02:49:01 +0000

    2007-01-30  Peter Williams  <peter@newton.cx>
    	* omega/layout.py (Grid,Overlay): Break out these classes into a
    	separate file...
    
    	* omega/base.py (Grid,Overlay): ... from here.
    
    	* omega/__init__.py: Modify to still import the above.
    
    
    
    git-svn-id: https://newton.cx/svn/trunk/omegaplot@779 69fc08fc-fdfc-0310-ace8-9e8da0474e22

--- omega/__init__.py
+++ omega/__init__.py
@@ -5,11 +5,18 @@ from base import *
 import bag
 import gtkThread
 import gtkUtil
+import layout
 import sources
 import stamps
 import styles
 
 from bag import Bag
+from layout import Overlay, Grid
 from styles import BlackOnWhiteBitmap, WhiteOnBlackBitmap
 from sources import Function
-from gtkUtil import LiveDisplay # FIXME: import different class depending on what toolkit is available
+
+# FIXME: we might want to import a different
+# class depending on what toolkit is available (eg,
+# if there is ever a qtUtil).
+
+from gtkUtil import LiveDisplay
