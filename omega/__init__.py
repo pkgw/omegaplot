af9cc6b9751f9533c109e166abeaa417a4cb128f
Peter Williams <peter@newton.cx>
2007-09-12 01:20:05 +0000

    2007-09-11  Peter Williams  <peter@newton.cx>
    	* omega/__init__.py (MQD): Ah, fuck it, commit this utility thingie.
    
    
    
    git-svn-id: https://newton.cx/svn/trunk/omegaplot@912 69fc08fc-fdfc-0310-ace8-9e8da0474e22

--- omega/__init__.py
+++ omega/__init__.py
@@ -15,3 +15,37 @@ from util import dump, LiveDisplay, PaintPipeline, LetterFile, InchesFile, Large
 QP = util.makeQuickPipeline
 QD = util.makeQuickDisplay
 RSP = util.makeRectSkeletonPipeline
+
+def MQD (*args, **kwargs):
+    if len (args) < 1: raise Exception ('dumbass')
+    
+    xmin = kwargs.get ('xmin')
+    xmax = kwargs.get ('xmax')
+    ymin = kwargs.get ('ymin')
+    ymax = kwargs.get ('ymax')
+
+    for s in ('xmin', 'xmax', 'ymin', 'ymax'):
+        if s in kwargs: del kwargs[s]
+    
+    (pl, rp) = RSP ()
+    tmpl = None
+    
+    for a in args:
+        if isinstance (a, tuple):
+            if len (a) != 2: raise Exception ('dumbass2')
+            tmpl = util.addQuickRectDataPainter (pl, rp, a[0], yinfo=a[1],
+                                                 tmpl=tmpl, **kwargs)
+        else:
+            tmpl = util.addQuickRectDataPainter (pl, rp, a, tmpl=tmpl, **kwargs)
+
+    if xmin is not None:
+        rp.defaultField.xaxis.min = xmin
+    if xmax is not None:
+        rp.defaultField.xaxis.max = xmax
+    if ymin is not None:
+        rp.defaultField.yaxis.min = ymin
+    if ymax is not None:
+        rp.defaultField.yaxis.max = ymax
+        
+    rp.magicAxisPainters ('lb')
+    return (pl.makeLiveDisplay (), pl, rp)
