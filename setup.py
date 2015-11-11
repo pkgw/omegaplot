298589d7a848cb35d6210bd2e98c1a8e3ca8325e
Peter Williams <peter@newton.cx>
2015-11-11 15:39:36 -0500

    setup.py: declare this to be 0.6.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup (
     name = 'omegaplot',
-    version = '0.5.99',
+    version = '0.6.0',
 
     # This package actually *is* zip-safe, but I've run into issues with
     # installing it as a Zip: in particular, the install sometimes fails with
